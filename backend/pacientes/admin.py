2573429562af5565f4f4b4f5c79c0a8490d744f9
ChorizoMagico <juanestebanarias.2006@gmail.com>
2026-07-13 18:59:55 -0500

    HU-004-1, HU-005-1, HU-007-1 hecho, modelos registrados en admin.py

--- backend/pacientes/admin.py
+++ backend/pacientes/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Paciente, EPS, Especialidad, Medico, Cita
 
 # Register your models here.
+
+admin.site.register(Paciente)
+admin.site.register(EPS)
+admin.site.register(Especialidad)
+admin.site.register(Medico)
+admin.site.register(Cita)
