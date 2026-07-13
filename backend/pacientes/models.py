5c7b35746c77750e4aaff66e3b8ffdffee3916a2
ChorizoMagico <juanestebanarias.2006@gmail.com>
2026-07-13 18:34:59 -0500

    HU-004-1, HU-005-1, HU-007-1 hecho

--- backend/pacientes/models.py
+++ backend/pacientes/models.py
@@ -35,3 +35,53 @@ class Paciente(models.Model):
 
     def __str__(self):
         return f"{self.usuario.first_name} {self.usuario.last_name}"
+    
+class Especialidad(models.Model):
+    """Modelo de especialidad médica"""
+    nombre = models.CharField(max_length=100, unique=True)
+    descripcion = models.TextField(blank=True)
+
+    class Meta:
+        db_table = 'especialidad'
+        verbose_name_plural = 'Especialidades'
+
+    def __str__(self):
+        return self.nombre
+
+class Medico(models.Model):
+    """Modelo de Médico"""
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    especialidades = models.ManyToManyField(Especialidad, related_name='medicos')
+    registro_medico = models.CharField(max_length=50, unique=True)
+    activo = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'medico'
+
+    def __str__(self):
+        return f"Dr. {self.usuario.last_name} - {self.usuario.first_name}"
+    
+class Cita(models.Model):
+    ESTADOS = [
+        ('PENDIENTE', 'Pendiente'),
+        ('CONFIRMADA', 'Confirmada'),
+        ('CANCELADA', 'Cancelada'),
+    ]
+
+    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE, related_name='citas')
+
+    medico = models.ForeignKey(Medico, on_delete=models.PROTECT)
+    especialidad = models.ForeignKey(Especialidad, on_delete=models.PROTECT)
+    
+    fecha_hora = models.DateTimeField()
+    estado = models.CharField(max_length=20, choices=ESTADOS, default='PENDIENTE')
+    motivo = models.TextField(blank=True, null=True)
+    creado_en = models.DateTimeField(auto_now_add=True)
+    actualizado_en = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'cita'
+        ordering = ['-fecha_hora']
+
+    def __str__(self):
+        return f"Cita {self.id} - {self.paciente} - {self.fecha_hora}"
